339a1fd5917a9999d1b2d6e62075065438c943e5
tecstudio <mmd206823@gmail.com>
2024-09-27 10:39:01 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
 "token": "", // token bot
-"prefix": "", // prefix bot
- "categoryId": "" // category ticketId
+"prefix": "-", // prefix bot
+ "categoryId": "1283361202604478515" // category ticketId
 }
